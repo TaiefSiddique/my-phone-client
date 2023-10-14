59e28414d2171f9f250c21b4e06edb42b7ad7d6c
TaiefSiddique <taiefsiddik2@gmail.com>
2023-10-14 17:19:51 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Main from './components/Main';
+import Phones from './components/Phones';
+import Phone from './components/Phone';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:
+      [
+        {
+          path: "/phones",
+          element: <Phones></Phones>,
+          loader: () => fetch('http://localhost:3000/phones')
+        },
+        {
+          path: "/phones/:id",
+          element: <Phone></Phone>,
+          loader: ({ params }) => fetch(`http://localhost:3000/phones/${params.id}`)
+        },
+      ]
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
